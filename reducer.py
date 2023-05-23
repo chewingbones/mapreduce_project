059c0c5b062f3af908968303140902de1fbe10d8
chewingbones <bobbywwilsonjr@gmail.com>
2023-05-22 21:50:24 -0700

    small update to reducer with sort function added

--- reducer.py
+++ reducer.py
@@ -7,6 +7,7 @@ import sys
 current_month = None
 current_count = 0
 month = None
+sorted_list = []
 
 # input comes from STDIN
 for line in sys.stdin:
@@ -26,11 +27,19 @@ for line in sys.stdin:
         current_count += count
     else:
         if current_month:
-            # write result to STDOUT
-            print ('%s\t%s' % (current_month, current_count))
+            # append to list to be sorted later
+            
+            sorted_list.append((current_month, current_count))
         current_count = count
         current_month = month
         
-
+#append final result 
 if current_month == month:
-    print ('%s\t%s' % (current_month, current_count))
+    
+    sorted_list.append((current_month, current_count))
+#sorts by total_price
+sorted_list = sorted(sorted_list, key=lambda x: x[1], reverse = True)
+#outputs all of the months; can easily be ammended to output largest month
+#we also wouldn't have to sort the list if we just want the largest value
+for item in sorted_list:
+    print ('%s\t%s' % (item[0], item[1]))
